a1c612c569e8292bc9cd26bde77711b2f0c86c99
Virgilio <47868066+Hecater88@users.noreply.github.com>
2022-03-25 14:21:27 +0100

    Pre-rendering, dynamic routes, API routes and deploy

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,41 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          Soy un programador front-end que se preocupa por la accesibilidad del
-          usuario y mira por el futuro de los proyectos, utilizo arquitectura de
-          css, código limpio y optimización del código para que el proyecto sea
-          escalable. Me apasiona los videojuegos, diseño y desarrollo
-          videojuegos en mis ratos libres. También soy programador Wordpress. Me
-          interesan las tecnologías y lenguajes de programación de desarrollo
-          web y aplicaciones.
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
